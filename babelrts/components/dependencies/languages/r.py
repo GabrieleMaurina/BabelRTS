9d98e8264af4705231ec67d4754b9da6d2cfe5d5
ammuaj <muaj.ruet@gmail.com>
2023-11-29 12:18:27 -0700

    added source pattern and actions for lanugage R

--- babelrts/components/dependencies/languages/r.py
+++ babelrts/components/dependencies/languages/r.py
@@ -1,13 +1,26 @@
-from babelrts.components.dependencies.token_language import TokenLanguage
+from babelrts.components.dependencies.language import Language
+from babelrts.components.dependencies.extension_pattern_action import ExtensionPatternAction
 
-class R(TokenLanguage):
+from re import compile as cmp_re
+from os.path import join, normpath, sep
 
-    def get_extensions(self):
-        return 'R'
+# it matches pattern like source("A1.R");source("file09.a.r") #files imported 
+# retrunr only the file  name like A1.R, file09.a.r
 
-    def get_tokens(self):
-        return ('source','library','require','use')
+SOURCE_PATTERN = cmp_re(r'source\s*\("([A-Za-z0-9_-.]+\.[R|r])"\)') 
+class R(Language):
+
+    def get_extensions_patterns_actions(self):
+        return ExtensionPatternAction('r', SOURCE_PATTERN, self.source_action)
 
     @staticmethod
     def get_language():
         return 'r'
+    
+    def source_action(self, match, file_path, folder_path, content):
+        print (match)
+        dependencies = set()
+        for folder in self.get_folders(folder_path):
+            if self.is_file(file:=join(folder, match)):
+                dependencies.add(self.check_two_way(file, file_path))
+        return dependencies
