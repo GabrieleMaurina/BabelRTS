9893fd1b74e800cb080059a041efa4b68c033a93
GabrieleMaurina <gabrielemaurina95@gmail.com>
2024-11-19 23:45:34 -0700

    Improved java.

--- babelrts/components/dependencies/languages/java.py
+++ babelrts/components/dependencies/languages/java.py
@@ -8,10 +8,12 @@ from pygtrie import StringTrie
 IMPORT_PATTERN = cmp_re(r'\bimport\s+(\S+)\s*;')
 IMPORT_STATIC_PATTERN = cmp_re(r'\bimport\s+static\s+(\S+)\s*;')
 PACKAGE_PATTERN = cmp_re(r'\bpackage\s+(\S+)\s*;')
-EXTENDS_PATTERN = cmp_re(r'\bextends\s+([\s\S]+?)\s*(?:{|implements)')
+EXTENDS_PATTERN = cmp_re(
+    r'\bextends\s+([\s\S]+?)\s*(?:<\S*>)?\s*(?:{|implements)')
 IMPLEMENTS_PATTERN = cmp_re(r'\bimplements\s+([\s\S]+?)\s*(?:{|extends)')
-NEW_PATTERN = cmp_re(r'\bnew\s+(\S+?)\s*\(\s*')
-STATIC_PATTERN = cmp_re(r'\b((?:[a-z0-9_\.]+\.)?[A-Z][A-Za-z0-9_]+?)\.[A-Za-z_]')
+NEW_PATTERN = cmp_re(r'\bnew\s+(\S+?)\s*(?:<\S*>)?\s*\(\s*')
+STATIC_PATTERN = cmp_re(
+    r'\b((?:[a-z0-9_\.]+\.)?[A-Z][A-Za-z0-9_]+?)\.[A-Za-z_]')
 ANNOTATION_PATTERN = cmp_re(r'(?<!\S)@(\w+)')
 THROWS_PATTERN = cmp_re(r'\bthrows\s+([A-Za-z_\., ]+?)\s*{')
 CATCH_PATTERN = cmp_re(r'\bcatch\s*\(\s*([A-Za-z_\.| ]+?)\s*\S+\)')
@@ -19,19 +21,28 @@ CATCH_PATTERN = cmp_re(r'\bcatch\s*\(\s*([A-Za-z_\.| ]+?)\s*\S+\)')
 SPLIT = cmp_re(r',|\||\n')
 SPLIT_PATH = cmp_re(r'\\|\/')
 
+
 class Java(Language):
     def get_extensions_patterns_actions(self):
         return (
             ExtensionPatternAction('java', IMPORT_PATTERN, self.import_action),
-            ExtensionPatternAction('java', IMPORT_STATIC_PATTERN, self.import_static_action),
-            #ExtensionPatternAction('java', PACKAGE_PATTERN, self.package_action),
-            ExtensionPatternAction('java', EXTENDS_PATTERN, self.multiple_used_classes_action),
-            ExtensionPatternAction('java', IMPLEMENTS_PATTERN, self.multiple_used_classes_action),
-            ExtensionPatternAction('java', NEW_PATTERN, self.used_class_action),
-            ExtensionPatternAction('java', STATIC_PATTERN, self.used_class_action),
-            ExtensionPatternAction('java', ANNOTATION_PATTERN, self.used_class_action),
-            ExtensionPatternAction('java', THROWS_PATTERN, self.multiple_used_classes_action),
-            ExtensionPatternAction('java', CATCH_PATTERN, self.multiple_used_classes_action)
+            ExtensionPatternAction(
+                'java', IMPORT_STATIC_PATTERN, self.import_static_action),
+            # ExtensionPatternAction('java', PACKAGE_PATTERN, self.package_action),
+            ExtensionPatternAction(
+                'java', EXTENDS_PATTERN, self.multiple_used_classes_action),
+            ExtensionPatternAction(
+                'java', IMPLEMENTS_PATTERN, self.multiple_used_classes_action),
+            ExtensionPatternAction('java', NEW_PATTERN,
+                                   self.used_class_action),
+            ExtensionPatternAction(
+                'java', STATIC_PATTERN, self.used_class_action),
+            ExtensionPatternAction(
+                'java', ANNOTATION_PATTERN, self.used_class_action),
+            ExtensionPatternAction(
+                'java', THROWS_PATTERN, self.multiple_used_classes_action),
+            ExtensionPatternAction('java', CATCH_PATTERN,
+                                   self.multiple_used_classes_action)
         )
 
     @staticmethod
@@ -56,9 +67,10 @@ class Java(Language):
         return self.class_to_files(match, file_path)
 
     def multiple_used_classes_action(self, match, file_path, folder_path, content):
-        clazzes = (clazz for clazz in (v.strip() for v in SPLIT.split(match)) if clazz)
+        clazzes = (clazz.split('<', 1)[0] for clazz in (v.strip()
+                   for v in SPLIT.split(match)) if clazz)
         return (file for clazz in clazzes for file in self.class_to_files(clazz, file_path))
-    
+
     def get_files_for_class(self, clazz, multiple):
         if multiple and self.classes.has_subtrie(clazz):
             tokens = len(clazz.split('.'))
@@ -87,8 +99,9 @@ class Java(Language):
             else:
                 self.classes[clazz] = [file]
             self.packages[file] = clazz.rsplit('.', 1)[0]
-    
+
     def _find_clazz(self, file, folders):
-        size = max([len(folder) for folder in folders if file.startswith(folder)])
+        size = max([len(folder)
+                   for folder in folders if file.startswith(folder)])
         clazz = file[size + 1:-5].replace(sep, '.')
         return clazz
